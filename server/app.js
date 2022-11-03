6a6810e5623b41cf115c478235410ff49db6f89d
IagoAntunes <iagoantunes.f@gmail.com>
2022-11-03 17:03:43 -0300

    list onlines

--- server/app.js
+++ server/app.js
@@ -4,11 +4,23 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-const messages = []
+const messages = [];
+var onlines = [];
+var allClients = [];
 
+var users =[];
 
 io.on('connection', (socket) => {
-  const username = socket.handshake.query.username
+  allClients.push(socket);
+  const username = socket.handshake.query.username;
+  const users = [];
+  for (let [id, socket] of io.of("/").sockets) {
+    users.push({
+      userID: id,
+      user: socket.handshake.query.username,
+    });
+  }
+  io.emit("users", users);
   socket.on('message', (data) => {
     const message = {
       msg: data['msg'],
@@ -19,10 +31,23 @@ io.on('connection', (socket) => {
     messages.push(message)
     io.emit('message', message)
 
-  })
+  });
   socket.on('online',(data) => {
-    
+    onlines.push({user: data['user']});
+    io.emit('online',onlines)
   })
+  socket.on("private message", ({ content, to }) => {
+    socket.to(to).emit("private message", {
+      content,
+      from: socket.id,
+    });
+  });
+  socket.on('disconnect', function() {
+    console.log('Got disconnect!');
+
+    var i = allClients.indexOf(socket);
+    allClients.splice(i, 1);
+  });
 });
 
 server.listen(4590, () => {
